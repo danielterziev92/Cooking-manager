945734923d5817a4d2a4ba632d8a513efc20fd27
Borislav Kamenov <kamenowink@gmail.com>
2023-12-15 14:24:11 +0200

    add BaseUser ADMIN

--- cooking_manager/cooking_manager/user_app/admin.py
+++ cooking_manager/cooking_manager/user_app/admin.py
@@ -1,37 +1,13 @@
-# from django.contrib import admin
-# from django.contrib.auth import admin as auth_admin, get_user_model
-# from django.utils.translation import gettext_lazy as _
-#
-# UserModel = get_user_model()
-#
-#
-# @admin.register(UserModel)
-# class UserAppAdmin(auth_admin.UserAdmin):
-#     change_user_password_template = None
-#     ordering = ('email',)
-#     list_display = ('email', 'is_active', 'date_joined')
-#     fieldsets = (
-#         (None, {'fields': ('email',)}),
-#         (
-#             _('Permissions'),
-#             {
-#                 'fields': (
-#                     'is_active',
-#                     'is_staff',
-#                     'is_superuser',
-#                     'groups',
-#                     'user_permissions',
-#                 ),
-#             },
-#         ),
-#         (_('Important dates'), {'fields': ('last_login',)}),
-#     )
-#     add_fieldsets = (
-#         (
-#             None,
-#             {
-#                 "classes": ('wide',),
-#                 "fields": ('email', 'password1', 'password2'),
-#             },
-#         ),
-#     )
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import BaseUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = BaseUser
+    list_display = ('email', 'is_active', 'is_staff', 'date_joined')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(BaseUser, CustomUserAdmin)
